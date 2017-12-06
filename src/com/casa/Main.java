edeeda65302e13f373ed7bdf737d5f4a99075948
diegodf8 <diegodf8@gmail.com>
2017-12-06 13:00:18 +0100

    Factorial

--- src/com/casa/Main.java
+++ src/com/casa/Main.java
@@ -1,13 +1,19 @@
 package com.casa;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int fact=1;
         int numero=7;
+        int numero2;
 
-        for (int i=1;i<=numero;i++){
+        Scanner reader = new Scanner (System.in);
+        numero2=reader.nextInt();
+
+        for (int i=1;i<=numero2;i++){
             fact*=i;
             System.out.println(fact);
         }
