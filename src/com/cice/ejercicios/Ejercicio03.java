d0a044ff3e962216cf6ebaa1a6a6abff2dc4c826
diegodf8 <diegodf8@gmail.com>
2017-12-08 12:57:56 +0100

    Capturar por pantalla 2

--- src/com/cice/ejercicios/Ejercicio03.java
+++ src/com/cice/ejercicios/Ejercicio03.java
@@ -1,9 +1,12 @@
 package com.cice.ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio03 {
     public static void main (String [] args ){
 
         int[] numeros = {10,20,36,49,56};
+        int numerotabla = 1;
 
         System.out.println("/////////////////////////////////////");
         for (int j =0; j < numeros.length; j++){
@@ -14,6 +17,16 @@ public class Ejercicio03 {
 
         for (int i: numeros){
             System.out.println(i);
+        }
+        System.out.print("Introduce numero para calcular su tabla de multiplicar: ");
+        Scanner sc = new Scanner(System.in);
+        numerotabla=sc.nextInt();
+
+
+
+        for (int k =0; k <=10; k++){
+            System.out.println(numerotabla +" * " + k + " " + (k*numerotabla));
+
         }
 
     }
---
+++ src/com/cice/ejercicios/Ejercicio04.java
@@ -0,0 +1,39 @@
+package com.cice.ejercicios;
+
+public class Ejercicio04 {
+    public static void main(String [] args){
+
+        double[] notas = {10, 6.7, 8,10, 10};
+        double notaMedia = 0;
+        double aux = 0 ;
+
+        for (double i : notas) {
+            aux+=i;
+        }
+
+        notaMedia = aux / notas.length;
+
+        System.out.println("La nota media es: " + notaMedia);
+
+        if (notaMedia < 5) {
+            System.out.println("INSUFICIENTE");
+        }
+        else if (notaMedia < 6 ){
+            System.out.println("SUFICIENTE");
+        }
+        else if (notaMedia < 7.5) {
+            System.out.println("BIEN");
+        }
+        else if (notaMedia <8.5) {
+            System.out.println("NOTABLE");
+        }
+        else {
+            System.out.println("SOBRESALIENTE");
+        }
+
+    }
+
+
+
+
+}
