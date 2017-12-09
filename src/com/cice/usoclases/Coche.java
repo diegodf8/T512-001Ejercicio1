eb502db7c1927ee5fadbbe637f88792a5fc8e130
diegodf8 <diegodf8@gmail.com>
2017-12-09 11:41:44 +0100

    Capturar por pantalla 2

---
+++ src/com/cice/usoclases/Coche.java
@@ -0,0 +1,24 @@
+package com.cice.usoclases;
+
+/**
+ * @autor Diego Dominguez
+ */
+
+
+public class Coche {
+
+    //Atributos
+    String nombre = "Alfred";
+    double altura = 1.72;
+    int vida = 100;
+
+    //Metodo --> ambito (public y private)  ;tipo de devolucion (int, double, float, boolean, void);
+    // Nombre del metodo (sin espacios ni caracteres especiales;
+    // psrsmetros entrada entre parentesis
+
+
+    public static void main(String[] args){
+        System.out.println("Tst");
+
+    }
+}
